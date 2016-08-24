b7733c88ee65c1395f4aecfa30a9021f043bac8f
vincent <vincent@gephen.co>
2016-08-24 18:35:15 +0800

    initial commit

---
+++ src/Main.elm
@@ -0,0 +1,26 @@
+module Main exposing (..)
+
+import Html.App
+import Messages exposing (Msg(..))
+import Models exposing (Model, initialModel)
+import View exposing (view)
+import Update exposing (update)
+import Players.Commands exposing(fetchAll)
+
+init : (Model, Cmd Msg)
+init =
+  (initialModel , Cmd.map PlayersMsg fetchAll)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+-- MAIN
+
+main =
+  Html.App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
